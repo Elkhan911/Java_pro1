af78f713b19b9d7abf7f1a82169c0ef9850349fa
Elkhan911 <955pro@mail.ru>
2023-03-04 21:29:28 +0500

    code2

--- project_one/src/App.java
+++ project_one/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("args");
+
+        System.out.println(1971-2023);
     }
 }
