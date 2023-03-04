864643a15e50dbb6a832d449c1df920ae1112e73
Elkhan911 <955pro@mail.ru>
2023-03-04 21:36:51 +0500

    code3

--- project_one/src/App.java
+++ project_one/src/App.java
@@ -4,5 +4,11 @@ public class App {
         System.out.println("args");
 
         System.out.println(1971 - 2023);
+
+        System.out.println("Привет!");
+                System.out.println("Эта строчка должна быть на новой строке!");
+        System.out.print("И эта, так как предыдущая команда выполнила переход на новую строку");          
+                         System.out.print("--- а это нет, так как просто System.out.print() по");
+        System.out.print("сле себя строку не переводит");
     }
 }
