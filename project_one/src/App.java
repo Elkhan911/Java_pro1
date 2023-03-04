e6cbe638c61b8b8dfcb9fa390998d562f2fab912
Elkhan911 <955pro@mail.ru>
2023-03-04 22:24:06 +0500

    task 2

--- project_one/src/App.java
+++ project_one/src/App.java
@@ -1,14 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("args");
 
-        System.out.println(1971 - 2023);
+        // 1. Напишите программу, которая выведет на экран 
+        // "Hello Programming!" (без кавычек) на 2 строках, по слову на каждой (см. пример вывода)
 
-        System.out.println("Привет!");
-                System.out.println("Эта строчка должна быть на новой строке!");
-        System.out.print("И эта, так как предыдущая команда выполнила переход на новую строку");          
-                         System.out.print("--- а это нет, так как просто System.out.print() по");
-        System.out.print("сле себя строку не переводит");
+        // 2. Напишите программу, которая выводит на экран строку "Hello, Java!" (без кавычек)
+        System.out.println("Hello, Java!");
+
+
     }
 }
