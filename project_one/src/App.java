a75f3560c7d8c75938c3d1fd19f87c901b831af9
Elkhan911 <955pro@mail.ru>
2023-03-06 23:13:55 +0500

    tasks 3-4

--- project_one/src/App.java
+++ project_one/src/App.java
@@ -1,12 +1,41 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        // 1. Напишите программу, которая выведет на экран 
-        // "Hello Programming!" (без кавычек) на 2 строках, по слову на каждой (см. пример вывода)
+    // 1. Напишите программу, которая выведет на экран
+    // "Hello Programming!" (без кавычек) на 2 строках, по слову на каждой (см.
+    // пример вывода)
+    System.out.println("Hello, Programming!");
 
-        // 2. Напишите программу, которая выводит на экран строку "Hello, Java!" (без кавычек)
-        System.out.println("Hello, Java!");
+    // 2. Напишите программу, которая выводит на экран строку "Hello, Java!" (без
+    // кавычек)
+    System.out.println("Hello, Java!");
 
+    // 3. Создайте две переменные: целого типа a и действительного b, равные 5 и 0.4
+    // соответственно. Выведите построчно на экран их сумму, разность, произведение
+    // и частное.
 
-    }
+    var a3 = 5;
+    var b3 = 0.4;
+    var sum3 = a3 + b3;
+    var subtraction3 = a3 - b3;
+    var multiplication3 = a3 * b3;
+    var division3 = a3 / b3;
+
+    System.out.println(sum3);
+    System.out.println(subtraction3);
+    System.out.println(multiplication3);
+    System.out.println(division3);
+
+    // 4. Напишите программу, которая считывает три целых числа и выводит их
+    // произведение.
+
+    var a4 = 10;
+    var b4 = 2;
+    var c4 = 3;
+
+    var multiplication4 = a4 * b4 * c4;
+
+    System.out.println(multiplication4);
+
+  }
 }
