5c15d0abc5338f59d8f5d1ace8589d92948c9e18
Elkhan911 <955pro@mail.ru>
2023-03-07 22:46:21 +0500

    task 9

--- project_one/src/App.java
+++ project_one/src/App.java
@@ -120,14 +120,5 @@ public class App {
       System.out.println(numb9B);
     }
 
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    if (a > b) {
-      System.out.println(a);
-    } else {
-      System.out.println(b);
-    }
-
   }
 }
